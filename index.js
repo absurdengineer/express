7e2c98455c58b89b8e01fd845a434dc4757875e4
mddalam1 <mddalam1@gmail.com>
2020-12-01 01:30:10 +0530

    Handled Each Get request in standard format and passed Status Codes

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ const express = require("express")
 
 const app = express()
 
+const courses = [
+    {courseId : 1, name : "Python"},
+    {courseId : 2, name : "JavaScript"},
+    {courseId : 3, name : "Java"},
+    {courseId : 4, name : "Spring"},
+    {courseId : 5, name : "MySQl"},
+]
+
 app.get("/",(req,res) => {
     res.send("Hello World")
 });
 
 app.get("/api/courses/",(req,res) => {
-    res.send([
-        {courseId : 1, name : "Python"},
-        {courseId : 2, name : "JavaScript"},
-        {courseId : 3, name : "Java"},
-        {courseId : 4, name : "Spring"},
-        {courseId : 5, name : "MySQl"},
-    ])
+    res.send(courses)
 });
 
 app.get("/api/course/:courseId/",(req,res) => {
-    const courseId = req.params.courseId
-    if(courseId == 1) res.send({courseId : 1, name : "Python"})
-    else if(courseId == 2) res.send({courseId : 2, name : "JavaScript"})
-    else if(courseId == 3) res.send({courseId : 3, name : "Java"})
-    else if(courseId == 4) res.send({courseId : 4, name : "Spring"})
-    else if(courseId == 5) res.send({courseId : 5, name : "MySQl"})
-    else res.send("Course Not Found")
+
+    let course = courses.find(c => c.courseId === parseInt(req.params.courseId))
+    if(!course) res.status(404).send("The course with given id doesn't exists.")
+    res.send(course)
+    
 });
 
 app.get("/api/posts/:year/:month/",(req,res) => {
