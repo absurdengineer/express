66b2651d1b7dd6a59ebfc636a692c24bcd48e192
mddalam1 <mddalam1@gmail.com>
2020-12-01 00:56:29 +0530

    Installed nodemon and done basic stuff related to get method

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ app.get("/",(req,res) => {
     res.send("Hello World")
 });
 
+app.get("/api/courses/",(req,res) => {
+    res.send([1,2,3,4,5])
+});
+
+app.get("/api/classes/",(req,res) => {
+    res.send(["First","Second","Third"])
+});
+
 app.listen(3000,() => { 
     console.log("Listening on port 3000... ")
     console.log("Server Started at http://127.0.0.1:3000/")
-    
+
 });
